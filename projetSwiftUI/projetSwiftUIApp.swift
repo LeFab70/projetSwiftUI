e7704f5b016fb5ccb48dfc530ef357e55a84eb23
LeFab70 <kouonang2002@gmail.com>
2025-09-08 08:23:34 -0300

    Initial Commit

---
+++ projetSwiftUI/projetSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  projetSwiftUIApp.swift
+//  projetSwiftUI
+//
+//  Created by Fabrice Kouonang on 2025-09-08.
+//
+
+import SwiftUI
+
+@main
+struct projetSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
